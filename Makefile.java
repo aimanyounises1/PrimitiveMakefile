f49fb7a11bf7e77efed74f40c8f549a540a92b82
aimanyounises1 <aymanyounis@icloud.com>
2020-08-16 05:36:43 +0300

    added upqrades

--- Makefile.java
+++ Makefile.java
@@ -8,11 +8,22 @@ import java.util.List;
 
 public class Makefile {
 	List <String> names;
+	List<String> headers;
 	public Makefile(String [] Make) throws IOException {
 	//buildCMakeFile(Make);
 		names = new LinkedList<String>();
+		headers = new LinkedList<String>();
 		for (int i = 0; i < Make.length; i++) {
-			names.add(Make[i]);
+			String s = Make[i];
+			boolean Cfile = s.charAt(s.length()-1) == 'h' ;
+			if(!Cfile) {
+			 names.add(Make[i]);
+			}
+		}
+		for (int i = 0; i < Make.length; i++) {
+			String s = Make[i];
+			if(s.contains(".h"))
+			headers.add(Make[i]);
 		}
 		
 	}
@@ -20,6 +31,8 @@ public class Makefile {
 		File f = new File("Makefile");
 		FileWriter fw = new  FileWriter(f);
 		BufferedWriter bw = new BufferedWriter(fw);
+		System.out.println(headers);
+		System.out.println(names);
 		bw.write("#!make -f\r\n");
 		bw.write("CC=gcc");
 		bw.newLine();
@@ -28,11 +41,21 @@ public class Makefile {
 			bw.write(string.substring(0, string.length() -2)+ " ");
 		}
 		for (int i = 0; i < names.size(); i++) {
+			String s = names.get(i);
 			bw.newLine();
 			bw.append(names.get(i).substring(0, names.get(i).length() -2)+":");
 			bw.newLine();
 			bw.append("\t");
-			bw.append("$(CC) -o " + names.get(i).substring(0, names.get(i).length() -2) +" "+names.get(i));
+			String ss = "";
+			for (int j = 0; j < names.size(); j++) {
+				String s1 = names.get(i).substring(0, names.get(i).length()-2);
+				s1 = s1 + ".h";
+				ss = headers.contains(s1) != false ?  s1 : "";
+			//	System.out.println(ss);
+				if(ss.length() > 0) break;
+			}
+			bw.append("$(CC) -o " + names.get(i).substring(0, names.get(i).length() -2) + " "
+			+names.get(i)+" "+ ss);
 		}
 		bw.newLine();
 		bw.append("clean");
@@ -52,24 +75,27 @@ public class Makefile {
 		bw.write("#!make -f\r\n");
 		bw.write("CC=clang++-9");
 		bw.newLine();
+		bw.write("CXXFLAGS=-std=c++2a\r\n");
+		bw.newLine();
 		bw.write("all:");
 		for (String string : names) {
-			bw.write(string.substring(0, string.length() -2)+ " ");
+			bw.write(string.substring(0, string.length() -4)+ " ");
 		}
+		bw.newLine();
 		for (int i = 0; i < names.size(); i++) {
 			bw.newLine();
-			bw.append(names.get(i).substring(0, names.get(i).length() -2)+":");
+			bw.append(names.get(i).substring(0, names.get(i).length() -4)+":");
 			bw.newLine();
 			bw.append("\t");
-			bw.append("$(CC) -o " + names.get(i).substring(0, names.get(i).length() -2) +" "+names.get(i));
+			bw.append("$(CC) $(CXXFLAGS) -o " + names.get(i).substring(0, names.get(i).length() -4) +" "+names.get(i));
 		}
 		bw.newLine();
 		bw.append("clean");
-		bw.newLine();
+		bw.newLine();	
 		bw.append("\t");
 		bw.append("rm -rf *.o ");
 		for (int i = 0; i < names.size(); i++) {
-			bw.append(names.get(i).substring(0, names.get(i).length()-2)+ " ");
+			bw.append(names.get(i).substring(0, names.get(i).length()-4)+ " ");
 		}
 		bw.close();
 		fw.close();
@@ -78,7 +104,7 @@ public class Makefile {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		String [] prog = {"filter.c","main.c","HelloWorld.c"};
+		String [] prog = {"filter.c","main.c", "filter.h"};
 		Makefile a = new Makefile(prog);
 		a.buildCMakeFile();
 		
